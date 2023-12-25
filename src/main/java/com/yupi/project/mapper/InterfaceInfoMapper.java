f71aba30eff678de68b830e5ee0d6de1c38e36ef
黄俊 <463252344@qq.com>
2023-12-25 14:36:08 +0800

    接口信息的crud

---
+++ src/main/java/com/yupi/project/mapper/InterfaceInfoMapper.java
@@ -0,0 +1,20 @@
+package com.yupi.project.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.yupi.project.model.entity.InterfaceInfo;
+import com.yupi.project.model.entity.Post;
+
+
+/**
+* @author 46325
+* @description 针对表【interface_info(接口信息)】的数据库操作Mapper
+* @createDate 2023-12-23 21:33:05
+* @Entity generator.domain.InterfaceInfo
+*/
+public interface InterfaceInfoMapper extends BaseMapper<InterfaceInfo> {
+
+}
+
+
+
+
